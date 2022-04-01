1e23abb0f51aefc9d259b35039d64bd959ce58f4
lyzpyb <lyzpyb@126.com>
2022-04-02 00:38:57 +0800

    mod

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+css: {
+    postcss: {
+      plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+      ],
+    },
+}
