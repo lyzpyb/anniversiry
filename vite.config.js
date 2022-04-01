80db2427f40eb85eeff3519b61f0014e72d72613
lyzpyb <lyzpyb@126.com>
2022-04-02 01:36:36 +0800

    2

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  return defineConfig({
+    base: loadEnv(mode, process.cwd()).VITE_BASE_URL,
+    plugins: [vue()],
+    css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  })
+}
+
